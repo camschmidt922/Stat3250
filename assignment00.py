61556922bdc27e57752404c1222bbc76417b9e48
camschmidt922 <134323274+camschmidt922@users.noreply.github.com>
2025-09-03 16:22:58 -0400

    Add files via upload

--- assignment00.py
+++ assignment00.py
@@ -21,28 +21,56 @@
 ##
 ## Find the sum of 23 and 119
 
-q1 = None
-
-#TEST
+q1 = 23+119
+## Using the basic math operations to find the given sum as 142. The solution appears in the
+## console, but if you are running the code as a whole rather than line-by-line (at least on
+## my IDE) you would need to print it out to see the solution with print(23+119).
 
 ## Question 2
 ##
 ## Create a list with "Hello" as the first element and "World" as the second element.
 
-q2 = None
+q2 = myList = ["Hello", "World"]
+## Created a list called myList that contains two strings, as indicated by the quotation marks.
+
 
 ## Question 3
-##
+
 ## Determine the first 5 prime numbers, in order.  Then give: 
 ##   (a) the largest of these 
 ##   (b) a list of the 1st, 2nd, and 4th of these (when small to large)
 ##   (c) a list of all 5 (smallest to largest)
 
+# Building my list that will store prime numbers
+primeList = []
+# Now a function to determine if numbers are prime
+def isPrime(n):
+    if n==1:
+        return False
+    if n==2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+# This while loop will fill primeList with prime numbers until it's length exceeds 4 (in other
+# words, is equal to 5)
+n=1
+while len(primeList) < 5:
+    if isPrime(n):
+        primeList.append(n)
+    n += 1
+
 ## 3(a)
-q3a = None
+# Taking index -1 from primeList, which gives the first item from the end
+q3a = primeList[-1]
 
 ## 3(b)
-q3b = None
+# Building a new list using indexes 0 (first item), 1 (second item), and 3 (fourth item)
+# from primeList
+q3b = [primeList[0], primeList[1], primeList[3]]
 
 ## 3(c)
-q3c = None
+# primeList is already in order from how I selected the first 5 prime numbers
+q3c = primeList
