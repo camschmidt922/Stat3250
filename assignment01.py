bc5b7bbbf36e22244351762d87ef886ecdbc6a77
camschmidt922 <134323274+camschmidt922@users.noreply.github.com>
2025-09-03 22:38:05 -0400

    Add files via upload

--- assignment01.py
+++ assignment01.py
@@ -2,6 +2,7 @@
 # # File: assignment01.py (STAT 3250)
 # # Topic: Assignment 1 
 # #
+from itertools import product
 
 # # Two *very* important rules that must be followed in order for your assignment
 # # to be graded correctly:
@@ -21,34 +22,63 @@ list04 = [3,5,-10,2,0,4,-5,-7,6,2,3,3,5,12,-5,-9,-7,4]
 biglist = list01 + list02 + list03 + list04
 
 # # 1.  Find the product of the last five elements of list04.
-
-q1 = None
+temp1 = list04[-5:]
+# Created a list with the last five elements of list04 using the colons and index to represent
+# the fifth to last value to the end of the list
+product = 1
+# Created an object that would become the final project. Starts at 1 because when I multiply it
+# with the first element of temp1, it will become that element.
+for i in temp1:
+    product *= i
+# Multiplies each element with product, which creates an iterative process that multiples
+# one times the first element times the second element and so forth
+q1 = product
 
 
 # # 2.  Extract the sublist of list01 that goes from
 # #      the 4th to the 10th elements (inclusive).
 
-q2 = None
+# Created a subset of list01 using a sequence of every integer from 3 to 10, not including 10.
+# This sequence is 3, 4, . . . 9. These are the indexes of elements 4 through 10 in list01.
+q2 = list01[3:10]
 
 
 # # 3.  Concatenate list01 and list04 (in that order), sort
 # #      the combined list, then extract the sublist that 
 # #      goes from the 6th to the 17th elements (inclusive).
 
-q3 = None
+temp2 = list01 + list04
+temp2.sort()
+# Created a new list in which I saved the concatenation of list01 and list04.
+# The new list was then sorted and had a subset from indexes 5 to 16 inclusive taken,
+# which corresponds to elements 6 to 17 inclusive.
+q3 = temp2[5:17]
 
 
 # # 4.  Generate "biglist", then extract the sublist of every 5th 
 # #      element starting with the 4th list element
 
-q4 = None
+temp3 = []
+# Created a new list that will become the sublist.
+for i in range(3, len(biglist), 5):
+    temp3.append(biglist[i])
+# This for loop starts at 3 and counts by 5 until it reaches the length of bigList. I use
+# the loop's index as an index for extracting values from bigList. It starts at the 4th element,
+# which is indexed by 3, and then every 5th element from there. It appends each selected value
+# to our new list, which becomes the desired sublist.
+q4 = temp3
 
 
 # # 5.  Determine the number of times that 3 appears in biglist.
 
-q5 = None
+# Using the count function on the list, which will return how many times the given parameter
+# appears in the list.
+q5 = biglist.count(3)
+
 
 
 # # 6.  Determine the index for the first appearance of 12 in biglist.
 
-q6 = None
+# Using the index function, which returns the index of the first appearance of the given
+# parameter in the list.
+q6 = biglist.index(12)
